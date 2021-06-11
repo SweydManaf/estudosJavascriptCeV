60c1d435cbe5fd9018e988c21d451bace3052d45
Sweyd Abdul <abdulsweyd@gmail.com>
2021-06-11 20:03:38 +0200

    repetições parte 1
    Modulo E
    Aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+/*
+console.log('tudo bem ')
+console.log('tudo bem ')
+console.log('tudo bem ')
+console.log('tudo bem ')
+console.log('tudo bem ')
+console.log('tudo bem ')
+*/
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+}
+*/
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+}
