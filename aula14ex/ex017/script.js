67145c69f8e1289f70f8085f8d3c2739b772da34
Sweyd Abdul <abdulsweyd@gmail.com>
2021-06-14 21:48:29 +0200

    exercicio 17 - tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert('Ola')
+function tabuada(params) {
+    let num = document.getElementById('num_id')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
+
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+
+        }
+    }
+
+}
